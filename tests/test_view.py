5f5741c86f57bd6951a26d3e9a8e734a18d43abe
Arthur Bueno Silva <arthurbueno1395271@hotmail.com>
2024-09-18 17:48:18 -0300

    :wrench: Fixing app module not found

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,4 +1,11 @@
+import os
+import sys
+
 import pytest
+
+# Add the parent directory to the path so that we can import the app module
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from mock.mock_communication import MockCommunication
 
 from app.chat.models import Chat
