e2481412b15950f12759be3f43df7bdaa84366c0
Arthur Bueno Silva <arthurbueno1395171@hotmail.com>
2024-11-26 14:19:10 -0300

    :bug: Fixed message not saving

--- app/chat/socketio_communication.py
+++ app/chat/socketio_communication.py
@@ -1,10 +1,12 @@
 from app.chat.communication import Communication
+from app.chat.models import Chat
 from app.extensions import socketio
 
 
 class SocketIOCommunication(Communication):
     def send_message(self, message: str, chat_id: int):
         socketio.emit("receive_message", {"message": message, "chat_id": chat_id})
+        Chat.save_message(message, chat_id, False)
 
     def finish_chat(self):
         socketio.emit("finished")
